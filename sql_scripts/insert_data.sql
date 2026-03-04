6d4602c45026525e254a1b68e11e0b546d812595
finnmrose <finnmrose2@gmail.com>
2026-03-04 13:04:51 +0000

    basic sql setup scripts

---
+++ sql_scripts/insert_data.sql
@@ -0,0 +1,4 @@
+USE `remixd`;
+
+INSERT INTO Account (Name, Email, Password) VALUES ('Rose', 'rose@email.com', 'password');
+INSERT INTO Review (AccountID, AlbumID, Score, Content) VALUES (1, 2130752, 10, 'still only like the third best kendrick lamar album lol');
