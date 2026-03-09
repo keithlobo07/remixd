0e7b768ba3c59a480a36163e9848eb6e2f0787b8
Keith Lobo <keithlobo0707@gmail.com>
2026-03-09 01:00:54 +0000

    Given login basic functionality.

---
+++ pages/general/login/login_script.js
@@ -0,0 +1,5 @@
+function returnLoginInfo() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    console.log({ email, password });
+}
