83ea5e065f0b1bf14268aeda0e00bca072329e0b
finnmrose <finnmrose2@gmail.com>
2026-04-03 19:56:22 +0100

    refactoring for templating + js requests

--- static/js/signup_script.js
+++ static/js/signup_script.js
@@ -1,7 +1,29 @@
-function returnSignupInfo() {
-    var email = document.getElementById("email").value;
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var confPassword = document.getElementById("confPassword").value;
-    console.log({ email, username, password, confPassword });
+function returnSignupInfo(url) {
+
+    const fd = new FormData(document.getElementById('signupForm'));
+
+    if (fd.get('password') != fd.get('confPassword')) {
+        console.error("passwords dont match");
+    } else {
+
+        fd.set('password', sha256(fd.get('password')))
+        fd.delete('confPassword')
+
+        const request = new Request(url, {
+            method:"PUT",
+            body:fd
+        });
+
+        console.log(request)
+
+        fetch(request).then(response => {
+            if (response.status == 201) {
+                fetch("/home").then(response => {
+                    window.location.href = response.url
+                })
+            } else {
+                console.log(response)
+            }
+        });
+    }
 }
